bcb25657d6e0da9ffeede4dd471401ee67d5ecb1
LuPeBreak <lfbmrj15@gmail.com>
2023-10-27 15:40:04 -0300

    Aula 1: - Separado responsabilidade de arquivos - criado worker para lidar com o processamento do video em segundo plano - implementado demuxer do video - implementado render do video no canvas

---
+++ project/app/pages/file-upload/src/app.js
@@ -0,0 +1,46 @@
+import Clock from "./deps/clock.js";
+import View from "./view.js";
+
+const view = new View();
+const clock = new Clock();
+let took = "";
+
+const worker = new Worker("./src/worker/worker.js", { type: "module" });
+
+worker.onerror = (error) => console.error("Error on Worker", error);
+
+worker.onmessage = ({ data }) => {
+  if (data.status !== "done") return;
+  clock.stop();
+  view.updateElapsedTime(`Process took ${took.replace("ago", "")}`);
+  console.log("recebi no processo da view", data);
+};
+
+view.configureOnFileChange((file) => {
+  const canvas = view.getCanvas();
+  worker.postMessage({ file, canvas }, [canvas]);
+
+  clock.start((time) => {
+    took = time;
+    view.updateElapsedTime(`Process started ${time}`);
+  });
+});
+
+async function fakeFetch() {
+  const filePath = "/videos/frag_bunny.mp4";
+  const response = await fetch(filePath);
+  // const response = await fetch(filePath,{
+  //   method: "HEAD"
+  // });
+  // traz o tamanho do arquivo
+  // response.headers.get('content-length')
+  const file = new File([await response.blob()], filePath, {
+    type: "video/mp4",
+    lastModified: Date.now(),
+  });
+  const event = new Event("change");
+  Reflect.defineProperty(event, "target", { value: { files: [file] } });
+  document.getElementById("fileUpload").dispatchEvent(event);
+}
+
+fakeFetch();
